1dde9c21191e020b276f528d0ff535e820f6e76c
NikolaEM <stankovic.nikola2@gmail.com>
2022-06-20 21:46:14 +0200

    AnimalList

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const AnimalList = [{
+  vrsta: 'zirafa',
+  ime: 'Doly',
+  datum_rodjenja: (new Date())
+},
+{
+  vrsta: 'lav',
+  ime: 'Pera',
+  datum_rodjenja: (new Date())},
+  {
+    vrsta: 'nosorog',
+    ime: 'Zile',
+    datum_rodjenja: (new Date())},
+    {
+      vrsta: 'kornjaca',
+      ime: 'Ana',
+      datum_rodjenja: (new Date())},
+      {
+        vrsta: 'krokodil',
+        ime: 'Rex',
+        datum_rodjenja: (new Date())}
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+    {AnimalList.map((animal, index) => (
+      <li
+      key={index}>{animal.vrsta}  {animal.ime} {animal.datum_rodjenja}
+      </li>
+      ))}
+      
+    </ul>
     </div>
   );
 }
